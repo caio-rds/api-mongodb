1e19a24d658fece92c179aa34f525c3442cac8ac
caiords <caiodtn@gmail.com>
2022-06-13 16:08:39 -0300

    functions working now

--- app/endpoints/mongodb/mdb_routes.py
+++ app/endpoints/mongodb/mdb_routes.py
@@ -1,22 +1,22 @@
 from fastapi import APIRouter
 from app.models.mongodb import *
-from app.controller.mongodb import MdbInterface
+from app.controller.mongodb import MdbController
 
 router = APIRouter()
-mdb_interface = MdbInterface()
+mdb_interface = MdbController()
 
 @router.get('/')
 async def get_mongodb(payload: Get):
     return await mdb_interface.get(payload)
 
 @router.post('/')
-async def post_mongodb():
-    return {'message': 'Hello World'}
+async def post_mongodb(payload: Post):
+    return await mdb_interface.post(payload)
 
 @router.put('/')
-async def put_mongodb():
-    return {'message': 'Hello World'}
+async def put_mongodb(payload: Put):
+    return await mdb_interface.put(payload)
 
 @router.delete('/')
-async def delete_mongodb():
-    return {'message': 'Hello World'}
+async def delete_mongodb(payload: Delete):
+    return await mdb_interface.delete(payload)
